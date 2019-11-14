5356b5f91597fd75cba69b40a2913a3c71d049a5
Miguel Torroja <miguel.torroja@gmail.com>
2019-11-14 07:19:10 +0100

    remote-p4: Sample test with capabilities

---
+++ contrib/vcs-p4/remote-p4.c
@@ -0,0 +1,109 @@
+#include "git-compat-util.h"
+#include "cache.h"
+#include "strbuf.h"
+#include "remote.h"
+
+
+#define DEBUG_VERBOSE
+#ifdef DEBUG_VERBOSE
+#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+static const char *remote_ref = "refs/heads/master";
+static const char *private_ref;
+
+static int cmd_capabilities(const char *line);
+static int cmd_import(const char *line);
+static int cmd_list(const char *line);
+
+typedef int (*input_command_handler)(const char *);
+
+
+struct input_command_entry {
+	const char *name;
+	input_command_handler fn;
+};
+
+static const struct input_command_entry input_command_list[] = {
+	{ "capabilities", cmd_capabilities},
+	{ "import", cmd_import},
+	{ "list", cmd_list},
+};
+
+static int cmd_capabilities(const char *line)
+{
+	printf("import\n");
+	printf("bidi-import\n");
+	printf("refspec %s:%s\n\n", remote_ref, private_ref);
+	fflush(stdout);
+	return 0;
+}
+
+static int cmd_import(const char *line)
+{
+	return 0;
+}
+
+static int cmd_list(const char *line)
+{
+	printf("? %s\n\n", remote_ref);
+	fflush(stdout);
+	return 0;
+}
+
+static int do_command(struct strbuf *line)
+{
+	const struct input_command_entry *p;
+	const struct input_command_entry * const p_end = input_command_list + ARRAY_SIZE(input_command_list);
+	for (p = input_command_list; p != p_end; p++) {
+		if (starts_with(line->buf, p->name) && (strlen(p->name) == line->len ||
+				line->buf[strlen(p->name)] == ' ')) {
+			return p->fn(line->buf);
+		}
+	}
+	die("Unknown command '%s'\n", line->buf);
+}
+
+int cmd_main(int argc, const char **argv)
+{
+	struct strbuf private_ref_sb = STRBUF_INIT;
+	struct strbuf buf = STRBUF_INIT;
+	struct remote *remote;
+	const char *url_in;
+	const char **arg_indx = NULL;
+	for (arg_indx = argv; arg_indx != argv + argc; arg_indx ++) {
+		LOG(" %s", *arg_indx);
+	}
+	LOG("\n");
+	setup_git_directory();
+	if (argc < 2 || argc > 3) {
+		usage("git-remote-p4 <remote-name> [user@perforce-server:perforce-port/master_branch]");
+		return 1;
+	}
+
+	remote = remote_get(argv[1]);
+	url_in = (argc == 3) ? argv[2] : remote->url[0];
+
+	strbuf_addf(&private_ref_sb, "refs/svn/%s/master", remote->name);
+	private_ref = private_ref_sb.buf;
+
+	while (1) {
+		if (strbuf_getline_lf(&buf, stdin) == EOF) {
+			if (ferror(stdin))
+				die("Error reading command stream");
+			else
+				die("Unexpected end of command stream");
+		}
+		LOG("command %s\n", buf.buf);
+		if (do_command(&buf))
+			break;
+		strbuf_reset(&buf);
+	}
+
+	strbuf_release(&buf);
+	strbuf_release(&private_ref_sb);
+
+	return 0;
+}
