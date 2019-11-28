e04dee668c222ba8ab32852fa8ada9a94eb55e25
Miguel Torroja <miguel.torroja@gmail.com>
2019-11-28 08:49:13 +0100

    remote-p4: printf instead of echo in t9850 test

--- t/t9850-md5.sh
+++ t/t9850-md5.sh
@@ -7,11 +7,11 @@ test_description='basic tests for the md5 checksum used by vcs-p4'
 test_expect_success 'md5 basic strings' '
 	test-md5 </dev/null >actual &&
 	grep d41d8cd98f00b204e9800998ecf8427e actual &&
-	echo -n "a" | test-md5 >actual &&
+	printf "a" | test-md5 >actual &&
 	grep 0cc175b9c0f1b6a831c399e269772661 actual &&
-	echo -n "abcdefghijklmnopqrstuvwxyz" | test-md5 >actual &&
+	printf "abcdefghijklmnopqrstuvwxyz" | test-md5 >actual &&
 	grep c3fcd3d76192e4007dfb496cca67e13b actual &&
-	echo -n "some random test" | test-md5 </dev/null >actual &&
+	printf "some random test" | test-md5 </dev/null >actual &&
 	test_must_fail grep 0123456789abcdefghijklmnopqrstuv actual
 '
 
